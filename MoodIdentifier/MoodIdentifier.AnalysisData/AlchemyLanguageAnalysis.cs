ac8705a29a4d47746b9f71d9f3ef5dfd3af9011a
Тоня <alaevat@yandex.ru>
2016-12-07 14:07:50 +0300

    Start analysis

---
+++ MoodIdentifier/MoodIdentifier.AnalysisData/AlchemyLanguageAnalysis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodIdentifier.AnalysisData
+{
+    class AlchemyLanguageAnalysis
+    {
+        const string AppId = "YOUR APPID GOES HERE";
+        //alchemy_language = AlchemyLanguageV1(api_key= 'API_KEY')
+        /*static string MakeAnalusis(string text)
+        {
+           // return string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&units=metric",
+             //    city, AppId);
+        }*/
+    }
+}
