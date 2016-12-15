13eaccf0d072c7f6f5ce884e627520f7997490c0
Тоня <alaevat@yandex.ru>
2016-12-16 00:22:47 +0300

    Add await

--- MoodIdentifier/MoodIdentifier.AnalysisData/RepositoryAnalysisData.cs
+++ MoodIdentifier/MoodIdentifier.AnalysisData/RepositoryAnalysisData.cs
@@ -15,22 +15,14 @@ namespace MoodIdentifier.AnalysisData
 {
     public class RepositoryAnalysisData
     {
-        public const string AppId = "375d440c74c17eba6676796f88ef68a2abb7fb14";
+        public const string AppId = "";
 
 
         public string CheckAnalysis(string text)
         {           
             text= HttpUtility.UrlEncode(text);
            
-            //return string.Format("https://watson-api-explorer.mybluemix.net/alchemy-api/calls/text/TextGetEmotion?apikey={0}&text={1}&outputMode=json",AppId,text);
-
-
-            //Если это правильно, то раскомментируйте, а строчку выше наоборот
-
-            //string RawData= await string.Format("https://watson-api-explorer.mybluemix.net/alchemy-api/calls/text/TextGetEmotion?apikey={0}&text={1}&outputMode=json", AppId, text);
-            // return (RawData);
-
-            
+            return string.Format("https://watson-api-explorer.mybluemix.net/alchemy-api/calls/text/TextGetEmotion?apikey={0}&text={1}&outputMode=json",AppId,text);
         }
 
 
@@ -40,21 +32,15 @@ namespace MoodIdentifier.AnalysisData
            
             using (var client = new HttpClient())
             {
-                var result = client.GetStringAsync(CheckAnalysis(text)).Result;
-                return JsonConvert.DeserializeObject<Results>(result);
+                var raw = await client.GetStringAsync(CheckAnalysis(text));
+                 var result = JsonConvert.DeserializeObject<Results>(raw);
+                
+                return result;
             }
-
-
-            
-
-
-
-
-
         }
-        
 
 
+        //работай прога
         public float FromStrToFloat(string str)
         {
            return( float.Parse(str.Replace('.', ',')));
@@ -84,8 +70,7 @@ namespace MoodIdentifier.AnalysisData
 
 
 
-        //этот метод интересен
-        //интересен?? лол
+        
         public Dictionary<DateTime, Answer> GetAnswer(Dictionary<DateTime,List<string>> SetOfTweets)  
         {
             Dictionary<DateTime, Answer> Answers = new Dictionary<DateTime, Answer>();
@@ -130,68 +115,8 @@ namespace MoodIdentifier.AnalysisData
             return (Answers); //метод возвращает словарь из (ключ - дата)-(начение-эмоция+цифраэмоции) по всем датам
         }
 
-        /*
-        foreach (List<string> SetOnedayTweet in SetOfTweets) //старый метод
-        {
-            int count = 0;
-            foreach (string TextOfTweet in SetOnedayTweet)
-            {
-
-                docEmotions RawAnalysis = (GetAnalysis(TextOfTweet).DocEmotions);
-                DataForAnalysis.Add(RawAnalysis);
-            }
-            EmotionOneDay emotionaloneday = new EmotionOneDay();
-            foreach (docEmotions OneTweetData in DataForAnalysis) // Суммирует значения каждой эмоции из каждого твитта
-            {
-
-                emotionaloneday.Anger = emotionaloneday.Anger + FromStrToFloat(OneTweetData.Anger);
-                emotionaloneday.Disgust = emotionaloneday.Disgust + FromStrToFloat(OneTweetData.Disgust);
-                emotionaloneday.Fear = emotionaloneday.Fear + FromStrToFloat(OneTweetData.Fear);
-                emotionaloneday.Joy = emotionaloneday.Joy + FromStrToFloat(OneTweetData.Joy);
-                emotionaloneday.Sadness = emotionaloneday.Sadness + FromStrToFloat(OneTweetData.Sadness);
-                count = count + 1;
-            }
-            SetAnalysisDate.Add(Сomputation(emotionaloneday, count));
-
-        }
-
-        return (SetAnalysisDate);
-
-        */
-
-
-
-        /*
-         //Старый метод очень старый
-        public Answer GetAnswer(List<TweetModel> SetOfTweets) //метод выдает самую сильную эмоцию которую считывает с массива из TweetModel. 
-        {
-            int count = 0;
-            List<docEmotions> DataForAnalysis = new List<docEmotions>();
-            foreach (TweetModel tweet in SetOfTweets)
-            {
-                string TextOfTweet = tweet.Text;
-                docEmotions RawAnalysis = (GetAnalysis(TextOfTweet).DocEmotions);
-                DataForAnalysis.Add(RawAnalysis);
-                
-            }
-            EmotionalsOneDay emotionaloneday = new EmotionalsOneDay();
-            foreach (docEmotions OneTweetData in DataForAnalysis) // Суммирует значения каждой эмоции из каждого твитта
-            {
-                
-                emotionaloneday.Anger = emotionaloneday.Anger + FromStrToFloat(OneTweetData.Anger);
-                emotionaloneday.Disgust=emotionaloneday.Disgust+ FromStrToFloat(OneTweetData.Disgust);
-                emotionaloneday.Fear = emotionaloneday.Fear + FromStrToFloat(OneTweetData.Fear);
-                emotionaloneday.Joy = emotionaloneday.Joy + FromStrToFloat(OneTweetData.Joy);
-                emotionaloneday.Sadness = emotionaloneday.Sadness + FromStrToFloat(OneTweetData.Sadness);
-                count = count + 1;
-            }
-
-            return (Сomputation(emotionaloneday, count));
-            
-
-        }*/
-
-
+        
+         
 
 
     }
